bfcfbca3e4a4eceb2ebc52820bfc0c9348d4f745
itsritesh02 <itsritesh02@gmail.com>
2026-08-03 17:59:28 +0530

    datatypes,js

---
+++ DataTypes/DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3)    //We are using node js not browser
+
+console.log(3 + 3);
+console.log("Rtesh");
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+
+//number 2 to the power 53
+// BigInt  
+//String =>" "
+// Boolean =>true/false
+// null => standalone value (empty value)
+//undefined
+//Symbol => Unique
+//object
+
+console.log(typeof"nitesh");        //==> String
+console.log(typeof age);           //==>number
+console.log(typeof null);         // ==> Object
+console.log(typeof undefined);   //==>undefined
